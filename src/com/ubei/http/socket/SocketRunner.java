ae460c87faf6a24f70ad0d2fa7649e17fb2fe127
Bektur777 <ubei003@mail.ru>
2023-12-26 22:47:22 +0600

    Add Socket class to work with tcp
    Using a socket to connect to a web server and send/receive data using the TCP principle

--- src/com/ubei/http/socket/SocketRunner.java
+++ src/com/ubei/http/socket/SocketRunner.java
@@ -1,8 +1,23 @@
 package com.ubei.http.socket;
 
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class SocketRunner {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+//        http - 80
+//        https - 443
+//        tcp
+        InetAddress inetAddress = Inet4Address.getByName("google.com");
+        try (Socket socket = new Socket(inetAddress, 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello, world!");
+            byte[] response = inputStream.readAllBytes();
+            System.out.println(response.length);
+        }
     }
 }
