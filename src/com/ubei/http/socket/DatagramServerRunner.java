782328c41bdfa59f05ab3290b180aefee9d20f16
Bektur777 <ubei003@mail.ru>
2023-12-27 15:19:49 +0600

    Add method to receive data using UDP protocol

--- src/com/ubei/http/socket/DatagramServerRunner.java
+++ src/com/ubei/http/socket/DatagramServerRunner.java
@@ -1,4 +1,19 @@
 package com.ubei.http.socket;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class DatagramServerRunner {
+
+
+    public static void main(String[] args) throws IOException {
+        try (var datagramSocket = new DatagramSocket(7777)) {
+            byte[] buffer = new byte[512];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(packet);
+
+            System.out.println(new String(packet.getData()));
+        }
+    }
 }
