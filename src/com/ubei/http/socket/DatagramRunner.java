0f2139fe5c5d61f117696cb1c7f28892222b692c
Bektur777 <ubei003@mail.ru>
2023-12-27 15:05:05 +0600

    Add DatagramSocket to send data using UDP protocol

---
+++ src/com/ubei/http/socket/DatagramRunner.java
@@ -0,0 +1,18 @@
+package com.ubei.http.socket;
+
+import java.io.IOException;
+import java.net.*;
+
+public class DatagramRunner {
+
+    public static void main(String[] args) throws IOException {
+        InetAddress inetAddress = InetAddress.getByName("localhost");
+
+        try (var datagramSocket = new DatagramSocket()) {
+//            ------> [buffer] <------
+            byte[] bytes = "Hello from UDP client".getBytes();
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, inetAddress, 7777);
+            datagramSocket.send(packet);
+        }
+    }
+}
