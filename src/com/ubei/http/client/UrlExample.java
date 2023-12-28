f059776ae7d1fb960c6d0ab1955b60072e1dee5c
Bektur777 <ubei003@mail.ru>
2023-12-28 21:04:04 +0600

    Add Url class
    Working with url class sending request to localhost files and to internet

--- src/com/ubei/http/client/UrlExample.java
+++ src/com/ubei/http/client/UrlExample.java
@@ -1,8 +1,27 @@
 package com.ubei.http.client;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class UrlExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("file:C:\\Users\\ubei0\\IdeaProjects\\http-servlets-starter\\src\\com\\ubei\\http\\socket\\DatagramRunner.java");
+        URLConnection urlConnection = url.openConnection();
 
+        System.out.println(new String(urlConnection.getInputStream().readAllBytes()));
+    }
+
+    private static void checkGoogle() throws IOException {
+        URL url = new URL("https://www.google.com");
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setDoOutput(true);
+
+        try (OutputStream outputStream = urlConnection.getOutputStream()) {
+        }
+
+        System.out.println();
     }
 }
