92c7859c85539418f10d0745355a3900eae550ae
Bektur777 <ubei003@mail.ru>
2023-12-29 16:56:57 +0600

    Create Http Server with connection

--- src/com/ubei/http/server/HttpServer.java
+++ src/com/ubei/http/server/HttpServer.java
@@ -1,2 +1,32 @@
-package com.ubei.http.server;public class HttpServer {
+package com.ubei.http.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class HttpServer {
+
+    private final int port;
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
+            processSocket(socket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void processSocket(Socket socket) {
+
+    }
 }
