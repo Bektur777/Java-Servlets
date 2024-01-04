3bf7d1e35a26583be5180a2620bbd2fae25e2ffc
Bektur777 <ubei003@mail.ru>
2024-01-04 17:58:35 +0600

    Add main methods

--- src/com/ubei/http/servlet/FirstServlet.java
+++ src/com/ubei/http/servlet/FirstServlet.java
@@ -3,11 +3,25 @@ package com.ubei.http.servlet;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class FirstServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
     }
 }
