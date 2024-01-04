d98c4c22e5f2f1c1ae9eed77b8a874094c49179d
Bektur777 <ubei003@mail.ru>
2024-01-04 17:52:37 +0600

    Init servlet FirstServlet class

---
+++ src/com/ubei/http/servlet/FirstServlet.java
@@ -0,0 +1,12 @@
+package com.ubei.http.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
