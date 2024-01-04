034080083d104dd9bd2e1da366619460fcd092b0
Bektur777 <ubei003@mail.ru>
2024-01-04 17:53:30 +0600

    Fix servlet init

--- src/com/ubei/http/servlet/FirstServlet.java
+++ src/com/ubei/http/servlet/FirstServlet.java
@@ -1,12 +1,13 @@
 package com.ubei.http.servlet;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 
 public class FirstServlet extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
-        super.init();
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
     }
 }
