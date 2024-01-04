c7389da20d33673a1e52f082c57fe184a4a941bd
Bektur777 <ubei003@mail.ru>
2024-01-04 19:33:03 +0600

    Add simple response for request

--- src/com/ubei/http/servlet/FirstServlet.java
+++ src/com/ubei/http/servlet/FirstServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
 
@@ -17,7 +18,10 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("<h1>Hello from first Servlet</h2>");
+        }
     }
 
     @Override
