66c833f23d48cb96fae7edc776b5a9b2951c424e
Bektur777 <ubei003@mail.ru>
2024-01-18 13:29:37 +0600

    Create DownloadServlet to download file

---
+++ src/com/ubei/http/servlet/DownloadServlet.java
@@ -0,0 +1,11 @@
+package com.ubei.http.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/download")
+public class DownloadServlet extends HttpServlet {
+
+
+
+}
