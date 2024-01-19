ba5e143052a9f7e7cff0759b6f97cbe395b5ce92
Bektur777 <ubei003@mail.ru>
2024-01-19 16:27:40 +0600

    Create FlightService to allocate and organize business logic

--- src/com/ubei/http/service/FlightService.java
+++ src/com/ubei/http/service/FlightService.java
@@ -4,6 +4,13 @@ import com.ubei.http.dao.FlightDao;
 
 public class FlightService {
 
+    private static final FlightService INSTANCE = new FlightService();
     private final FlightDao flightDao = FlightDao.getInstance();
 
+    private FlightService() {
+    }
+
+    public static FlightService getInstance() {
+        return INSTANCE;
+    }
 }
