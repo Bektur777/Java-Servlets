8cf422b9bdce4c8c2fc50084b3802442c99fb0cf
Bektur777 <ubei003@mail.ru>
2024-01-19 20:21:04 +0600

    Fix bug with postgresql driver
    Tomcat couldn't load the postgresql driver. Because of this, I had to connect directly in the class

--- src/com/ubei/http/util/ConnectionManager.java
+++ src/com/ubei/http/util/ConnectionManager.java
@@ -6,20 +6,32 @@ import java.sql.SQLException;
 
 public final class ConnectionManager {
 
-    private static final String URL_KEY = PropertiesUtil.get("db.url");
+    private static final String URL_KEY = "db.url";
+    private static final String USER_KEY = "db.user";
+    private static final String PASSWORD_KEY = "db.password";
 
-    private static final String USER_KEY= PropertiesUtil.get("db.user");
+    static {
+        loadDriver();
+    }
 
-    private static final String PASSWORD_KEY = PropertiesUtil.get("db.password");
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ConnectionManager() {
     }
 
     public static Connection get() {
         try {
-            return DriverManager.getConnection(URL_KEY, USER_KEY, PASSWORD_KEY);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USER_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
+        } catch (SQLException throwable) {
+            throw new RuntimeException(throwable);
         }
-    }
-}
+    }}
