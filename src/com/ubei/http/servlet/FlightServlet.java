55c172491c1d864c1e52bd5598d09efb81e71db7
Bektur777 <ubei003@mail.ru>
2024-01-19 16:32:51 +0600

    Create FlightServlet

---
+++ src/com/ubei/http/servlet/FlightServlet.java
@@ -0,0 +1,11 @@
+package com.ubei.http.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/flights")
+public class FlightServlet extends HttpServlet {
+
+
+
+}
