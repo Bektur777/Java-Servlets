00b5ac5d904b0a86b021704f774c8a809e4ae82a
Bektur777 <ubei003@mail.ru>
2024-01-19 15:48:53 +0600

    Add find all flights feature
    The buildFlight method has also been added, which is responsible for mapping between the table in the db and the entity. Will be remade in the future

--- src/com/ubei/http/dao/FlightDao.java
+++ src/com/ubei/http/dao/FlightDao.java
@@ -1,15 +1,36 @@
 package com.ubei.http.dao;
 
 import com.ubei.http.entity.Flight;
+import com.ubei.http.entity.FlightStatus;
+import com.ubei.http.util.ConnectionManager;
 
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class FlightDao implements Dao<Long, Flight> {
 
+    private static final String FIND_ALL = """
+            SELECT *
+            FROM flight
+            """;
+
     @Override
     public List<Flight> findAll() {
-        return null;
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Flight> flights = new ArrayList<>();
+            while (resultSet.next()) {
+                flights.add(buildFlight(resultSet));
+            }
+
+            return flights;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -30,5 +51,18 @@ public class FlightDao implements Dao<Long, Flight> {
     @Override
     public Flight save(Flight entity) {
         return null;
+    }
+
+    private Flight buildFlight(ResultSet resultSet) throws SQLException {
+        return new Flight(
+                resultSet.getObject("id", Long.class),
+                resultSet.getObject("flight_no", String.class),
+                resultSet.getObject("departure_date", Timestamp.class).toLocalDateTime(),
+                resultSet.getObject("departure_airport_code", String.class),
+                resultSet.getObject("arrival_date", Timestamp.class).toLocalDateTime(),
+                resultSet.getObject("arrival_airport_code", String.class),
+                resultSet.getObject("aircraft_id", Long.class),
+                FlightStatus.valueOf(resultSet.getObject("status", String.class))
+        );
     }
 }
