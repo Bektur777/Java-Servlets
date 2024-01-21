777e5cf0be17953c8a4019ddeee6ff6303df55c7
Bektur777 <ubei003@mail.ru>
2024-01-21 17:45:18 +0600

    Add find all tickets by flight id feature

--- src/com/ubei/http/dao/TicketDao.java
+++ src/com/ubei/http/dao/TicketDao.java
@@ -1,7 +1,14 @@
 package com.ubei.http.dao;
 
 import com.ubei.http.entity.Ticket;
+import com.ubei.http.util.ConnectionManager;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,8 +16,32 @@ public class TicketDao implements Dao<Long, Ticket> {
 
     private static final TicketDao INSTANCE = new TicketDao();
 
+    private static final String FIND_ALL_BY_FLIGHT_ID = """
+        SELECT *
+        FROM tickets
+        WHERE id=?
+    """;
+
     private TicketDao() {
     }
+
+    public List<Ticket> findAllByFlightId(Long flightId) {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_BY_FLIGHT_ID)) {
+            preparedStatement.setObject(1, flightId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Ticket> tickets = new ArrayList<>();
+            while (resultSet.next()) {
+                tickets.add(buildTicket(resultSet));
+            }
+
+            return tickets;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public List<Ticket> findAll() {
@@ -39,5 +70,20 @@ public class TicketDao implements Dao<Long, Ticket> {
 
     public static TicketDao getInstance() {
         return INSTANCE;
+    }
+
+    private Ticket buildTicket(ResultSet resultSet) {
+        try {
+            return new Ticket(
+                    resultSet.getObject("id", Long.class),
+                    resultSet.getObject("passenger_no", String.class),
+                    resultSet.getObject("passenger_name", String.class),
+                    resultSet.getObject("flight_id", Long.class),
+                    resultSet.getObject("seat_no", String.class),
+                    resultSet.getObject("cost", BigDecimal.class)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
