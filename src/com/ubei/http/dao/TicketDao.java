6f79b9004ddb0b919d405297a20f8ff47aeb9d40
Bektur777 <ubei003@mail.ru>
2024-01-21 17:12:22 +0600

    Create TicketDao and implements Dao methods

---
+++ src/com/ubei/http/dao/TicketDao.java
@@ -0,0 +1,43 @@
+package com.ubei.http.dao;
+
+import com.ubei.http.entity.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TicketDao implements Dao<Long, Ticket> {
+
+    private static final TicketDao INSTANCE = new TicketDao();
+
+    private TicketDao() {
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Ticket> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public void update(Ticket entity) {
+
+    }
+
+    @Override
+    public Ticket save(Ticket entity) {
+        return null;
+    }
+
+    public static TicketDao getInstance() {
+        return INSTANCE;
+    }
+}
