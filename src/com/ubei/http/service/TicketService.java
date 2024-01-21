7a6bf467fc08904bdef29009a5b187b74ad1a09f
Bektur777 <ubei003@mail.ru>
2024-01-21 17:46:03 +0600

    Add method to get all flight tickets by flight id

--- src/com/ubei/http/service/TicketService.java
+++ src/com/ubei/http/service/TicketService.java
@@ -1,10 +1,28 @@
 package com.ubei.http.service;
 
+import com.ubei.http.dao.TicketDao;
+import com.ubei.http.dto.TicketDto.TicketDto;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class TicketService {
 
     private static final TicketService INSTANCE = new TicketService();
+    private final TicketDao ticketDao = TicketDao.getInstance();
 
     private TicketService() {
+    }
+
+    public List<TicketDto> findAllFlightByFlightId(Long flightId) {
+        return ticketDao.findAllByFlightId(flightId).stream()
+                .map(ticket -> new TicketDto(
+                        ticket.getId(),
+                        ticket.getFlightId(),
+                        ticket.getSeatNo()
+                ))
+                .collect(toList());
     }
 
     public static TicketService getInstance() {
