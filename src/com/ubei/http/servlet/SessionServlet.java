8367ffab95cb3fa394dc1a3a12d901de0fb50dc0
Bektur777 <ubei003@mail.ru>
2024-01-22 15:08:38 +0600

    Add creating session feature

--- src/com/ubei/http/servlet/SessionServlet.java
+++ src/com/ubei/http/servlet/SessionServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class SessionServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        HttpSession session = req.getSession();
+        System.out.println(session.isNew());
     }
 }
