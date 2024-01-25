3426299ce255b8f17680cef1a3afb4570d40a744
Bektur777 <ubei003@mail.ru>
2024-01-25 18:46:43 +0600

    Implement save method in UserDao for User entity
    Extend the UserDao class with a functional implementation of the save method to persist User entities to the database. The method utilizes a SQL INSERT statement to store user data in the 'users' table, leveraging the ConnectionManager for database connectivity. The save method sets parameters for the PreparedStatement, executes the query, and retrieves the generated keys. The UserDao now includes a static INSTANCE field and a getInstance() method for a singleton pattern. This commit establishes the foundation for saving user data and interacting with the underlying database.

--- src/com/ubei/http/dao/UserDao.java
+++ src/com/ubei/http/dao/UserDao.java
@@ -1,13 +1,24 @@
 package com.ubei.http.dao;
 
 import com.ubei.http.entity.User;
+import com.ubei.http.util.ConnectionManager;
+import lombok.SneakyThrows;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Optional;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class UserDao implements Dao<Integer, User>{
 
     private static final UserDao INSTANCE = new UserDao();
+
+    private static final String SAVE_SQL =
+            "INSERT INTO users (name, birthday, email, password, role, gender) VALUES " +
+                    "(?, ?, ?, ?, ?, ?)";
 
     @Override
     public List<User> findAll() {
@@ -30,8 +41,25 @@ public class UserDao implements Dao<Integer, User>{
     }
 
     @Override
+    @SneakyThrows
     public User save(User entity) {
-        return null;
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SQL, RETURN_GENERATED_KEYS)) {
+            preparedStatement.setObject(1, entity.getName());
+            preparedStatement.setObject(2, entity.getBirthday());
+            preparedStatement.setObject(3, entity.getEmail());
+            preparedStatement.setObject(4, entity.getPassword());
+            preparedStatement.setObject(5, entity.getRole());
+            preparedStatement.setObject(6, entity.getGender());
+
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            generatedKeys.next();
+            generatedKeys.getObject("id", Integer.class);
+
+            return entity;
+        }
     }
 
     public static UserDao getInstance() {
