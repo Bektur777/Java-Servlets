cca8a6dd7fbeb668099319bf5bd44607110bdcd4
Bektur777 <ubei003@mail.ru>
2024-01-25 18:34:39 +0600

    Create User class

---
+++ src/com/ubei/http/entity/User.java
@@ -0,0 +1,22 @@
+package com.ubei.http.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    Integer id;
+    String name;
+    LocalDate birthday;
+    String email;
+    String password;
+    Role role;
+    Gender gender;
+}
