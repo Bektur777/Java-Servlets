7dd2c650732cf81e6a8bb0e0c73256261d0f905b
Bektur777 <ubei003@mail.ru>
2024-01-25 14:59:38 +0600

    Add feature to redirect client to registration form

--- src/com/ubei/http/servlet/RegistrationServlet.java
+++ src/com/ubei/http/servlet/RegistrationServlet.java
@@ -1,5 +1,6 @@
 package com.ubei.http.servlet;
 
+import com.ubei.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("roles", List.of("USER", "ADMIN"));
+        req.setAttribute("genders", List.of("MALE", "FEMALE"));
 
+        req.getRequestDispatcher(JspHelper.getPath("registration"))
+                .forward(req, resp);
     }
 }
