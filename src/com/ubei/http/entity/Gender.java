74fd4e4165fe60ad49cb0d7cf6cf1e99f80dee9c
Bektur777 <ubei003@mail.ru>
2024-01-28 13:32:46 +0600

    Enhance Gender enum with find method
    Extend the enumeration by gender by adding a static find method that allows you to extract the enumeration by gender based on its name. The find method uses Java Streams and Optional, which provides a more flexible and readable way to search an enumeration by gender by name. This is done because the Gender.valueOf(String name) method can return an exception. So we are working with null safety

--- src/com/ubei/http/entity/Gender.java
+++ src/com/ubei/http/entity/Gender.java
@@ -1,6 +1,15 @@
 package com.ubei.http.entity;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum Gender {
     MALE,
-    FEMALE
+    FEMALE;
+
+    public static Optional<Gender> find(String name) {
+        return Arrays.stream(values())
+                .filter(gender -> gender.name().equals(name))
+                .findFirst();
+    }
 }
