659dad296e167ba0c07b6b9d7809cb24640d446a
Bektur777 <ubei003@mail.ru>
2024-01-29 12:35:06 +0600

    Enhance UserService with user creation functionality
    Extend the UserService class by introducing a create method for handling the creation of users. The method validates the input Create UserDto using the CreateUserValidator, throwing a ValidationException if any errors are detected. If validation is successful, the Create UserDto is converted to a User entity using the User Mapper, and the user is saved to the database through the UserDao.

--- src/com/ubei/http/service/UserService.java
+++ src/com/ubei/http/service/UserService.java
@@ -1,5 +1,12 @@
 package com.ubei.http.service;
 
+import com.ubei.http.dao.UserDao;
+import com.ubei.http.dto.CreateUserDto;
+import com.ubei.http.entity.User;
+import com.ubei.http.exception.ValidationException;
+import com.ubei.http.mapper.UserMapper;
+import com.ubei.http.validator.CreateUserValidator;
+import com.ubei.http.validator.ValidationResult;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +14,20 @@ import lombok.NoArgsConstructor;
 public class UserService {
 
     private static final UserService INSTANCE = new UserService();
+    private final CreateUserValidator createUserValidator = CreateUserValidator.getInstance();
+    private final UserDao userDao = UserDao.getInstance();
+    private final UserMapper userMapper = UserMapper.getInstance();
 
+    public Integer create(CreateUserDto userDto) {
+        ValidationResult valid = createUserValidator.isValid(userDto);
+        if (!valid.isValid()) {
+            throw new ValidationException(valid.getErrors());
+        }
+        User user = userMapper.mapFrom(userDto);
+        userDao.save(user);
 
+        return user.getId();
+    }
 
     public static UserService getInstance() {
         return INSTANCE;
