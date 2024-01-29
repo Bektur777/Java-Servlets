5ef855aacbc61aca7f890816c608b8a86ae7e7c2
Bektur777 <ubei003@mail.ru>
2024-01-29 20:03:19 +0600

    Create ImageService for handling image uploads
    The class includes a method, upload, which takes an image path and input stream and saves the image content to the specified path. The base path is retrieved from configuration using PropertiesUtil.

---
+++ src/com/ubei/http/service/ImageService.java
@@ -0,0 +1,33 @@
+package com.ubei.http.service;
+
+import com.ubei.http.util.PropertiesUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ImageService {
+
+    public static final ImageService INSTANCE = new ImageService();
+    private final String basePath = PropertiesUtil.get("image.base.url");
+
+    @SneakyThrows
+    public void upload(String imagePath, InputStream imageContent) {
+        Path imageFullPath = Path.of(basePath, imagePath);
+
+        try (imageContent) {
+            Files.createDirectories(imageFullPath.getParent());
+            Files.write(imageFullPath, imageContent.readAllBytes(), CREATE, TRUNCATE_EXISTING);
+        }
+    }
+
+    public static ImageService getInstance() {
+        return INSTANCE;
+    }
+}
