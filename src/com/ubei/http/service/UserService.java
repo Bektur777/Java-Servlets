91a76d5971fde1976381ab66573177563e8e3bf6
Bektur777 <ubei003@mail.ru>
2024-01-29 20:10:29 +0600

    Integrate image upload in UserService

--- src/com/ubei/http/service/UserService.java
+++ src/com/ubei/http/service/UserService.java
@@ -9,21 +9,26 @@ import com.ubei.http.validator.CreateUserValidator;
 import com.ubei.http.validator.ValidationResult;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserService {
 
     private static final UserService INSTANCE = new UserService();
+
     private final CreateUserValidator createUserValidator = CreateUserValidator.getInstance();
     private final UserDao userDao = UserDao.getInstance();
     private final UserMapper userMapper = UserMapper.getInstance();
+    private final ImageService imageService = ImageService.getInstance();
 
+    @SneakyThrows
     public Integer create(CreateUserDto userDto) {
         ValidationResult valid = createUserValidator.isValid(userDto);
         if (!valid.isValid()) {
             throw new ValidationException(valid.getErrors());
         }
         User user = userMapper.mapFrom(userDto);
+        imageService.upload(user.getImage(), userDto.getImage().getInputStream());
         userDao.save(user);
 
         return user.getId();
