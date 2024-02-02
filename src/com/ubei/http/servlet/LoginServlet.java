30a38ade207909c623b755b6fd9a4664ecd132d8
Bektur777 <ubei003@mail.ru>
2024-02-02 15:43:38 +0600

    Add method to handling with login

--- src/com/ubei/http/servlet/LoginServlet.java
+++ src/com/ubei/http/servlet/LoginServlet.java
@@ -1,16 +1,21 @@
 package com.ubei.http.servlet;
 
+import com.ubei.http.dto.UserDto;
+import com.ubei.http.service.UserService;
 import com.ubei.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 
 import java.io.IOException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private final UserService userService = UserService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,7 +25,22 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
+        userService.login(req.getParameter("email"), req.getParameter("password"))
+                .ifPresentOrElse(
+                        userDto -> onLoginSuccess(userDto, req, resp),
+                        () -> onLoginFail(req, resp)
+                );
+    }
+
+    @SneakyThrows
+    private void onLoginSuccess(UserDto user, HttpServletRequest req, HttpServletResponse resp) {
+        req.getSession().setAttribute("user", user);
+        resp.sendRedirect("/flights");
+    }
+
+    @SneakyThrows
+    private void onLoginFail(HttpServletRequest req, HttpServletResponse resp) {
+        req.setAttribute("error", "Wrong email or password!");
+        doGet(req, resp);
     }
 }
