bbad71cb8145a5afe0735be3af9d6b4d06dad9d5
Bektur777 <ubei003@mail.ru>
2024-02-02 15:37:02 +0600

    Update file

---
+++ src/com/ubei/http/mapper/CreateUserMapper.java
@@ -0,0 +1,30 @@
+package com.ubei.http.mapper;
+
+import com.ubei.http.dto.CreateUserDto;
+import com.ubei.http.entity.Gender;
+import com.ubei.http.entity.Role;
+import com.ubei.http.entity.User;
+import com.ubei.http.util.LocalDateFormatter;
+
+public class CreateUserMapper implements Mapper<CreateUserDto, User> {
+
+    public static final String IMAGE_FOLDER = "users/";
+    public static final CreateUserMapper INSTANCE = new CreateUserMapper();
+
+    @Override
+    public User mapFrom(CreateUserDto object) {
+        return User.builder()
+                .name(object.getName())
+                .image(IMAGE_FOLDER + object.getImage().getSubmittedFileName())
+                .email(object.getEmail())
+                .gender(Gender.valueOf(object.getGender()))
+                .birthday(LocalDateFormatter.format(object.getBirthday()))
+                .role(Role.valueOf(object.getRole()))
+                .password(object.getPassword())
+                .build();
+    }
+
+    public static CreateUserMapper getInstance() {
+        return INSTANCE;
+    }
+}
