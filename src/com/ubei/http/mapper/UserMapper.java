371d4379e5da0b51d0982485a2e636d8419f26fe
Bektur777 <ubei003@mail.ru>
2024-02-02 15:38:40 +0600

    Add user mapping from user to dto

--- src/com/ubei/http/mapper/UserMapper.java
+++ src/com/ubei/http/mapper/UserMapper.java
@@ -1,30 +1,25 @@
 package com.ubei.http.mapper;
 
-import com.ubei.http.dto.CreateUserDto;
-import com.ubei.http.entity.Gender;
-import com.ubei.http.entity.Role;
+import com.ubei.http.dto.UserDto;
 import com.ubei.http.entity.User;
-import com.ubei.http.util.LocalDateFormatter;
 
-public class UserMapper implements Mapper<CreateUserDto, User> {
+public class UserMapper implements Mapper<User, UserDto> {
 
-    public static final String IMAGE_FOLDER = "users/";
-    public static final UserMapper userMapper = new UserMapper();
+    public static final UserMapper INSTANCE = new UserMapper();
 
     @Override
-    public User mapFrom(CreateUserDto object) {
-        return User.builder()
+    public UserDto mapFrom(User object) {
+        return UserDto.builder()
                 .name(object.getName())
-                .image(IMAGE_FOLDER + object.getImage().getSubmittedFileName())
+                .image(object.getImage())
                 .email(object.getEmail())
-                .gender(Gender.valueOf(object.getGender()))
-                .birthday(LocalDateFormatter.format(object.getBirthday()))
-                .role(Role.valueOf(object.getRole()))
-                .password(object.getPassword())
+                .gender(object.getGender())
+                .birthday(object.getBirthday())
+                .role(object.getRole())
                 .build();
     }
 
     public static UserMapper getInstance() {
-        return userMapper;
+        return INSTANCE;
     }
 }
