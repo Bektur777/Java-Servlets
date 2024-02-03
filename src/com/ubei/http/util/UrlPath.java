7d57cff92d6569b477fe4dd7ce7f65328f324763
Bektur777 <ubei003@mail.ru>
2024-02-03 14:22:54 +0600

    Create url paths

---
+++ src/com/ubei/http/util/UrlPath.java
@@ -0,0 +1,11 @@
+package com.ubei.http.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UrlPath {
+
+    public static final String LOGIN = "/login";
+    public static final String REGISTER = "/registration";
+    public static final String IMAGES = "/images";
+}
